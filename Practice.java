127e0850032ffd30955c0301e023995650b614a6
Aditee Jha <aditeejha2005@gmail.com>
2026-02-05 18:08:54 +0530

    changes

--- Practice.java
+++ Practice.java
@@ -1,3 +1,10 @@
-public class Practice {
-    
+import java.util.*;
+class LinearSearcgProgram{
+    static int linearSearch(int[] arr, int key){
+        int index=-1;
+        for (int if(arr[i ==key= 0; if(arr[i ==key< arr.length; if(arr[i+==key+) {
+            Object elem = arr[if(arr[i]==key;
+            
+        }}
+    }
 }
