5c1026d20632676942135a38da875279bfdf3446
Aditee Jha <aditeejha2005@gmail.com>
2026-02-22 11:07:24 +0530

    changes

---
+++ A_Extremely_Round.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class A_Extremely_Round {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int count=0;
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            for(int i=0; i<n; i++){
+                arr[i]=i+1;
+                int cnt=0;
+                while(arr[i]>0){
+                    int ld=arr[i]%10;
+                    if(ld!=0) cnt++;
+                    arr[i]=arr[i]/10;
+                    if(cnt>1) break;
+                }
+                if(cnt==1) count++;
+            }
+            System.out.println(count);
+        }
+    }
+}
