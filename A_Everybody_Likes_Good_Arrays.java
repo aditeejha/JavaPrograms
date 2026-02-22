e83d717bd9a04a5ef486521faed4f5b8216bf74d
Aditee Jha <aditeejha2005@gmail.com>
2026-02-22 11:37:51 +0530

    changes

--- A_Everybody_Likes_Good_Arrays.java
+++ A_Everybody_Likes_Good_Arrays.java
@@ -10,18 +10,19 @@ public class A_Everybody_Likes_Good_Arrays{
             for (int i = 0;i <n;i++) {   
                 list.add(sc.nextInt()); 
             }
-            if(list.xsize()==1){
+            if(list.size()==1){
                 System.out.println(0);
                 continue;
             }   
-            for(int i=0;i<list.size();i++){
-                if((list.get(i)%2==0 && list.get(i+1)%2==0) || (list.get(i)%2!=0 && list.get(i+1)%2!=0)){
+            for(int i=0;i<list.size()-1;i++){
+                if((list.get(i)+list.get(i+1))%2!=0){
                     continue;
                 }
                 else{
                     list.set(i, list.get(i)*list.get(i+1));
                     list.remove(i+1);
                     operations++;
+                    i--;
                 }
             }
             System.out.println(operations);
