7e318b38a195df16aed3cae092013c93449d6e23
Aditee Jha <aditeejha2005@gmail.com>
2026-02-23 16:35:14 +0530

    changes

--- A_Extremely_Round.java
+++ A_Extremely_Round.java
@@ -6,19 +6,21 @@ public class A_Extremely_Round {
         while(t-->0){
             int count=0;
             int n=sc.nextInt();
-            int[] arr=new int[n];
-            for(int i=0; i<n; i++){
-                arr[i]=i+1;
-                int cnt=0;
-                while(arr[i]>0){
-                    int ld=arr[i]%10;
-                    if(ld!=0) cnt++;
-                    arr[i]=arr[i]/10;
-                    if(cnt>1) break;
-                }
-                if(cnt==1) count++;
-            }
-            System.out.println(count);
+            // int[] arr=new int[n];
+            // for(int i=0; i<n; i++){
+            //     arr[i]=i+1;
+            //     int cnt=0;
+            //     while(arr[i]>0){
+            //         int ld=arr[i]%10;
+            //         if(ld!=0) cnt++;
+            //         arr[i]=arr[i]/10;
+            //         if(cnt>1) break;
+            //     }
+            //     if(cnt==1) count++;
+            // }
+            // System.out.println(count);
+            if(n<=10) count=n; 
+            
         }
     }
 }
