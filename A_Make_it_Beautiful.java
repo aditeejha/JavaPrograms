be22baf7f219cd97fd3be618dabb94d9d5f44e8f
Aditee Jha <aditeejha2005@gmail.com>
2026-03-02 17:39:13 +0530

    changes

--- A_Make_it_Beautiful.java
+++ A_Make_it_Beautiful.java
@@ -1,25 +1,25 @@
 import java.util.*;
-public class A_Make_it_Beautiful{
+public class A_Make_it_Beautiful {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t-->0){
-            int n=sc.nextInt();
-            int flag=0;
-            int sum=0;
-            int[] arr=new int[n];
-            for (int i = 0; i < arr.length; i++) {
-                arr[i]=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++){
+                arr[i] = sc.nextInt();
             }
-            for (int i=0; i<n-1; i++) {
-                sum+=arr[i];
-                if(sum==arr[i+1]){
-                    flag=1;
-                    break;
+            Arrays.sort(arr);
+            if(arr[0] == arr[n-1]){
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+                System.out.print(arr[n-1] + " ");
+                for(int i = 0; i < n-1; i++){
+                    System.out.print(arr[i] + " ");
                 }
-                
-            }}
+                System.out.println();
+            }
         }
     }
 }
-
---
+++ A_One_and_Two.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class A_One_and_Two {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0){
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            for (int i=0; i<n; i++){
+                arr[i]=sc.nextInt();
+            }
+        }
+    }
+}
