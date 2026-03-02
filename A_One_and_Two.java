0811b58277485d3eb25cdd599631305ef5ad1304
Aditee Jha <aditeejha2005@gmail.com>
2026-03-02 18:01:05 +0530

    changes

--- A_One_and_Two.java
+++ A_One_and_Two.java
@@ -11,10 +11,10 @@ public class A_One_and_Two {
                 arr[i]=sc.nextInt();
             }
             int prod=1;
-            int p=1;
             for (int i=0; i<n-1; i++){
                 prod*=arr[i];
                 k++;
+                int p=1;
                 for(int j=i+1; j<n; j++){
                     p*=arr[j];
                 }
