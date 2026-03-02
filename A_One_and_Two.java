8229f6d3f1d0948912f25b0f4f24f19becdb6cad
Aditee Jha <aditeejha2005@gmail.com>
2026-03-02 18:23:05 +0530

    changes

--- A_One_and_Two.java
+++ A_One_and_Two.java
@@ -4,8 +4,23 @@ public class A_One_and_Two {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
         while(t-- >0){
-           
-               
+           int n=sc.nextInt(); 
+           int TotTwo=0;
+           int[] arr=new int[n];
+           for(int i=0; i<n; i++){
+               arr[i]=sc.nextInt();
+               if(arr[i]==2) TotTwo++;
+           }  
+           if(TotTwo%2!=0) System.out.println(-1);
+           int target=TotTwo/2; 
+           int k=0;
+           for (int i=0; i<n; i++) {
+                if(arr[i]==2) k++;
+                if(k==target){
+                     System.out.println(i+1);
+                     break;
+                }
+           }
         }
     }
 }
