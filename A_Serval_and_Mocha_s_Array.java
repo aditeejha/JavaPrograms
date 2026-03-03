1d14f930b97d022766df90543687a91905b9b1e2
Aditee Jha <aditeejha2005@gmail.com>
2026-03-03 10:01:57 +0530

    changes

---
+++ A_Serval_and_Mocha_s_Array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class A_Serval_and_Mocha_s_Array{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            for (int i=0; i<n; i++) {
+                arr[i]=sc.nextInt();
+            }
+        }
+    }
+}
