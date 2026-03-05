76d9329afb7523f6d491cf132f55aa0e3f462c33
Aditee Jha <aditeejha2005@gmail.com>
2026-03-05 17:04:28 +0530

    changes

--- A_Walking_Master.java
+++ A_Walking_Master.java
@@ -1,1 +1,23 @@
 import java.util.*;
+public class A_Walking_Master{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int[] arr=new int[4];
+            for (int i = 0; i <4; i++) {
+                arr[i]=sc.nextInt();
+            }
+            if(arr[0]==arr[2] && arr[1]==arr[3]){
+                System.out.println(0);
+            }
+            else if(arr[3]<arr[1]){
+                System.out.println(-1);
+            }
+            else{
+                int ans=(arr[3]-arr[1])+(arr[2]-arr[0]);
+                System.out.println(ans);
+            }
+        }
+    }
+}
