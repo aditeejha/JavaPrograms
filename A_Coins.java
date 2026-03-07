e673c5d83ee15ce9ed047901821e978da42648e0
Aditee Jha <aditeejha2005@gmail.com>
2026-03-07 21:46:05 +0530

    changes

--- A_Coins.java
+++ A_Coins.java
@@ -7,21 +7,7 @@ public class A_Coins{
             int n=sc.nextInt();
             int k=sc.nextInt();
             int x,y;
-            if(k%2==0){
-                //x=k/2;
-                //y=k/2;
-                System.out.println("YES");
-            }
-            else{
-                //x=k/2+1;
-                //y=k/2;
-                if(n%2==0){
-                    System.out.println("NO");
-                }
-                else{
-                    System.out.println("YES");
-                }
-            }
+           
         }
     }
 }
