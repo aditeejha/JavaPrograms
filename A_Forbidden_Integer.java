f363f6eaa9dae3e01b8d4894b597922c0742f84f
Aditee Jha <aditeejha2005@gmail.com>
2026-03-11 18:41:00 +0530

    changes

---
+++ A_Forbidden_Integer.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class A_Forbidden_Integer{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int x=sc.nextInt();
+        }
+    }
+}
