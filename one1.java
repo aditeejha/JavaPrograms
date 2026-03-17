db3e6e9df93fcc51e64e24647cacb37ae5577b27
Aditee Jha <aditeejha2005@gmail.com>
2026-03-17 21:53:25 +0530

    changes

--- one1.java
+++ one1.java
@@ -5,23 +5,20 @@ public class one1{
       System.out.println("Enter the shape whose area you want to calculate");
       System.out.println("Enter 1 for square, 2 for rectangle, 3 for circle, 4 for triangle");
       int choice=sc.nextInt();
-      Switch(choice){
+      switch(choice){
         case 1:
             System.out.println("Enter side length");
             int side=sc.nextInt();
             System.out.println(side*side);
-      }
       case 2:
             System.out.println("Enter length and breadth");
             int l=sc.nextInt();
             int b=sc.nextInt();
             System.out.println(l*b);
-      }
       case 3:
             System.out.println("Enter radius");
             int r=sc.nextInt();
             System.out.println(3.14*r*r);
-      }
       case 4:
             System.out.println("Enter height and base");
             int h=sc.nextInt();
