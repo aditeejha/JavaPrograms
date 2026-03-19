ad84dd3370875242fbad387decedd76ee8a8cbca
Aditee Jha <aditeejha2005@gmail.com>
2026-03-19 19:44:59 +0530

    changes

--- Stack.java
+++ Stack.java
@@ -2,5 +2,24 @@ public class Stack {
     class StImpl{
         int top=-1; 
         int[] st=new int[10];
+        void push(int x){
+            if(top>=10){
+                System.out.println("Stack is full");
+            }
+            st[top]=x;
+        }
+        int topp(){
+            if(top==-1) return -1;
+            return st[top];
+        }
+        void pop(){
+            if(top==-1){
+                System.out.println("Stack is empty");
+            }
+            top=top-1;
+        }
+        int size(){
+            return top+1;
+        }
     }
 }
