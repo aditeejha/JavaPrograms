05a429304cb554b84ca93a9d38339032d3c66e1e
Aditee Jha <aditeejha2005@gmail.com>
2026-03-21 12:59:27 +0530

    changes

--- Queue.java
+++ Queue.java
@@ -1,3 +1,15 @@
 public class Queue{
-    
+    int size=10, currSize=0, start=-1, end=-1;
+    int[] q=new int[size];
+    void push(int x){
+        if(currSize==size)
+            System.out.println("Queue is full");
+        else if(currSize==0){
+            start=0; end=0;
+        }
+        else{
+            end=(end+1)%size;
+            q[end]=x;
+        }
+        }
 }
